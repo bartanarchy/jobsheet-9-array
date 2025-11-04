9c749f57e9719dd232a84a5a0950a47d5bb4cedc
nabatikeju1 <bart.playgame10@gmail.com>
2025-11-04 08:33:30 +0700

    original code

---
+++ arrayValue24.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class arrayValue24 {
+   public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int[] finalScores = new int[10];
+
+      for (int i = 0; i < 10; i++) {
+         System.out.print("Enter the final score " + i + ":");
+         finalScores[i] = sc.nextInt();
+      }
+      for (int i = 0; i < 10; i++) {
+         System.out.println("Final score " + i + " is " + finalScores[i]);
+      }
+   }
+}
