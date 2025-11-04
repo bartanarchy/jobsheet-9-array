daa2d67c8d6de5f67558b9564155528914497ddb
nabatikeju1 <bart.playgame10@gmail.com>
2025-11-04 09:48:17 +0700

    finish

--- arrayAverageScore24.java
+++ arrayAverageScore24.java
@@ -2,19 +2,40 @@ import java.util.Scanner;
 public class arrayAverageScore24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] score = new int[10];
+
+        System.out.print("Enter the number of students: ");
+        int n = sc.nextInt();
+
+        int[] score = new int[n];
         double total = 0;
         double average;
 
+        double totalPass = 0;
+        double totalFail = 0;
+        int passCount = 0;
+        int failCount = 0;
+
         for (int i = 0; i < score.length; i++){
-            System.out.print("Enter student score " + (i + 1) + ": ");
+            System.out.print("Enter the final score " + i + ": ");
             score[i] = sc.nextInt();
-        }
-        for (int i = 0; i < score.length; i++){
-            total += score[i];
+
+            total = total + score[i];
+
+            if(score[i] > 70){
+                totalPass = totalPass + score[i];
+                passCount = passCount + 1;
+            } else {
+                totalFail = totalFail + score[i];
+                failCount = failCount + 1;
+            }
         }
 
         average = total / score.length;
-        System.out.println("The class average score is: " + average);
+
+        System.out.println("The average class score is: " + average);
+        System.out.println("The average score of students who passed is " + (totalPass / passCount));
+        System.out.println("The average score of students who failed is " + (totalFail / failCount));
+
+        sc.close();
     }
 }
