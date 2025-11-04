57fac2564bc4d09e8fb2af97317f7fba82ad0db7
nabatikeju1 <bart.playgame10@gmail.com>
2025-11-04 10:16:07 +0700

    linear search last edit

--- linearSearch24.java
+++ linearSearch24.java
@@ -1,16 +1,57 @@
+import java.util.Scanner;
+
 public class linearSearch24 {
     public static void main(String[] args) {
-        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-        int key = 20;
-        int result = 0;
+        Scanner input = new Scanner(System.in);
+        // System.out.print("Enter the number of array elements: ");
+        // int n = input.nextInt();
 
-        for (int i = 0; i < arrayInt.length; i++) {
+        // int[] arrayInt = new int[n];
+
+        // for (int i = 0; i < n; i++) {
+        // System.out.print("Enter the array element " + i + ": ");
+        // arrayInt[i] = input.nextInt();
+        // }
+
+        // System.out.print("Enter the key you want to search for: ");
+        // int key = input.nextInt();
+
+        // boolean found = false;
+        // for (int i = 0; i < n; i++) {
+        // if (arrayInt[i] == key) {
+        // System.out.println("The key in the array is located at index position " + i);
+        // found = true;
+        // }
+        // }
+
+        // if (!found) {
+        // System.out.println("The key was not found in the array.");
+        // }
+        System.out.print("Enter the number of array elements: ");
+        int n = input.nextInt();
+
+        int[] arrayInt = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the array element " + i + ": ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Enter the key you want to search for: ");
+        int key = input.nextInt();
+
+        boolean found = false;
+        for (int i = 0; i < n; i++) {
             if (arrayInt[i] == key) {
-                result = i;
-                break;
+                System.out.println("The key in the array is located at index position " + i);
+                found = true;
             }
         }
 
-        System.out.println("The key in the array is located at index position " + result);
+        if (!found) {
+            System.out.println("Key not found");
+        }
+
+        input.close();
     }
 }
