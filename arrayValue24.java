ef7e1e31ca0cc8c9ff4e5de01c3e4e8910dd52f1
nabatikeju1 <bart.playgame10@gmail.com>
2025-11-04 09:11:37 +0700

    passed output

--- arrayValue24.java
+++ arrayValue24.java
@@ -4,12 +4,14 @@ public class arrayValue24 {
       Scanner sc = new Scanner(System.in);
       int[] finalScores = new int[10];
 
-      for(int i = 0; i < finalScores.length; i++){
-         System.out.print("Enter the Final score " + i + ": ");
+      for (int i = 0; i < 10; i++) {
+         System.out.print("Enter the final score " + i + ":");
          finalScores[i] = sc.nextInt();
       }
-      for (int i = 0; i < 10; i++) {
-         System.out.println("Final score " + i + " is " + finalScores[i]);
+      for (int i = 0; i < finalScores.length; i++) {
+         if (finalScores[i] > 70) {
+            System.out.println("Student " + i + " Passed! ");
+         }
       }
    }
 }
